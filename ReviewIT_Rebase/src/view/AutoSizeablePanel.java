4563cc133f042cc973da52beebd686b0f43a0252
Dimitar Kumanov <dimitark@uw.edu>
2017-05-18 19:37:41 -0700

    Added a javadoc

--- ReviewIT_Rebase/src/view/AutoSizeablePanel.java
+++ ReviewIT_Rebase/src/view/AutoSizeablePanel.java
@@ -6,6 +6,16 @@ import java.beans.PropertyChangeListener;
 
 import javax.swing.JPanel;
 
+
+/**
+ * An custom JPanel class with automatic resizeable behavior.
+ * Objects whose classes extend this class will always maintain a
+ * proper X(theXRatio) and Y(theYRatio) ratio relative to theStartingSize.
+ * In order for this to automatically resize it needs to be hooked up to a
+ * component which fires property change listener with the new dimension.
+ * @author Dimitar Kumanov
+ *
+ */
 public abstract class AutoSizeablePanel extends JPanel implements PropertyChangeListener {
 	private static final long serialVersionUID = -3723466386912611355L;
 	
